8cb680e23b1882295c94de0dd15793bd6090a15f
f.guglielmucci <148483575+fg-44@users.noreply.github.com>
2024-01-22 16:28:48 +0100

    update - mysidebar

--- src/components/MySidebar.jsx
+++ src/components/MySidebar.jsx
@@ -1,24 +1,53 @@
 import React from "react";
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
+import { FaPencilAlt } from "react-icons/fa";
+import { Button, Card, Col } from "react-bootstrap";
 
 function MySidebar() {
-
   return (
-    <Col xs={3}> 
-    <Card  className="my-3" style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+    <Col xs={3}>
+      <Col>
+        <Card className="my-3" style={{ width: "18rem" }}>
+          <Card.Body className="p-2" >
+          
+              <Card.Header className="bg-transparent d-flex justify-content-between">
+              <div>
+                <Card.Title className="h5">Lingua del profilo</Card.Title>
+                <Card.Subtitle className="my-2 text-muted h6">
+                  Card Subtitle
+                </Card.Subtitle>
+                </div>
+                <div className="d-flex justify-content-end align-items-start border-0">
+                  <Button className="bg-transparent text-dark d-flex justify-content-end align-items-end p-0 border-0">
+                    <FaPencilAlt />
+                  </Button>
+                </div>
+              </Card.Header>
 
-      </Card.Body>
-    </Card>
-  </Col>
-  )
+              <Card.Footer className="bg-transparent d-flex justify-content-between">
+                <div>
+                <Card.Title className="h5">Public profile & URL</Card.Title>
+                <Card.Subtitle className="my-2 text-muted h6">
+                  Card Subtitle
+                </Card.Subtitle>
+                </div>
+                <div className="d-flex justify-content-end align-items-start border-0">
+                  <Button className="bg-transparent text-dark d-flex justify-content-end align-items-end p-0 border-0">
+                    <FaPencilAlt />
+                  </Button>
+                </div>
+              </Card.Footer>
+
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col>
+        <Card style={{ width: "18rem", height: "16rem" }}>
+          {/*CARD IMAGE */}
+        </Card>
+      </Col>
+    </Col>
+  );
 }
 
 export default MySidebar;
