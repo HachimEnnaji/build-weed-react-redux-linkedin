4cc611a72d4dfe4aad7ea90308e268e807e70065
DaveGio11 <145689622+DaveGio11@users.noreply.github.com>
2024-01-22 12:58:17 +0100

    navbar-main

--- src/components/MyMain.jsx
+++ src/components/MyMain.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import logo from "../LinkedIn_icon.svg.png";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { FaPencilAlt } from "react-icons/fa";
 
 function MyMain() {
-  return <div>MyMain</div>;
+  return (
+    <Container>
+      <Row>
+        <Col xs={9}>
+          <Card className="m-3">
+            <Card.Img variant="top" src={logo} height={200} className="object-fit-cover" />
+            <Card.Body>
+              <div className="d-flex justify-content-between mb-3">
+                <div>
+                  <img
+                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="user"
+                    width={150}
+                    height={150}
+                    className="rounded-circle object-fit-cover ms-3"
+                    style={{ position: "absolute", top: 100, left: 0 }}
+                  />
+                </div>
+                <div>
+                  <FaPencilAlt />
+                </div>
+              </div>
+              <Row>
+                <Col xs={9}>
+                  <h3>Nome Cognome</h3>
+                  <p>Lavora presso</p>
+                  <span>Lavora spanresso</span>
+                  <a href="#">Informazioni di contatto</a>
+                </Col>
+                <Col xs={3}>hello</Col>
+              </Row>
+              <Card.Text></Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={3}>tizio</Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default MyMain;
--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -2,7 +2,12 @@ import React, { useState } from "react";
 import logo from "../LinkedIn_icon.svg.png";
 import { Button, Container, Form, Nav, NavDropdown, Navbar, Offcanvas } from "react-bootstrap";
 import { FaSearch } from "react-icons/fa";
-import { FaCaretDown } from "react-icons/fa";
+import { FaCaretDown, FaUserCircle } from "react-icons/fa";
+import { AiFillHome } from "react-icons/ai";
+import { HiUsers } from "react-icons/hi2";
+import { BsHandbagFill } from "react-icons/bs";
+import { BiSolidMessageRoundedDots, BiSolidGrid } from "react-icons/bi";
+import { FaBell } from "react-icons/fa";
 
 function MyNavbar() {
   const [show, setShow] = useState(false);
@@ -10,58 +15,83 @@ function MyNavbar() {
   const handleShow = () => setShow(true);
 
   return (
-    <Container>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container fluid>
-          <img src={logo} alt="logo-linkedin" style={{ height: "35px" }} className="mx-2 " />
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder=" 🔍  Search"
-              className="me-2 "
-              style={{ maxHeight: "100px" }}
-              aria-label="Search"
-            />
-            {/* onSubmit={} */}
-            {/* <FaSearch className="magnifying-icon" /> */}
-          </Form>
-          <Container className="px-5">
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-              <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
-                <Nav.Link href="#action1">Home</Nav.Link>
-                <Nav.Link href="#action2">Rete</Nav.Link>
-                <Nav.Link href="#action2">Lavoro</Nav.Link>
-                <Nav.Link href="#action2">Messaggistica</Nav.Link>
-                <Nav.Link href="#action2">Rete</Nav.Link>
+    <Navbar expand="lg" className="bg-white" sticky="top">
+      <Container>
+        <img src={logo} alt="logo-linkedin" style={{ height: "35px" }} className="mx-2 " />
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder=" 🔍  Search"
+            className="me-2 navPlaceholder"
+            style={{ maxHeight: "100px" }}
+            aria-label="Search"
+          />
+          {/* onSubmit={} */}
+          {/* <FaSearch className="magnifying-icon" /> */}
+        </Form>
+        <Container className="px-5">
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
+              <Nav.Link href="#action1" className="me-3 text-center">
+                <AiFillHome className="fs-3" />
+                Home
+              </Nav.Link>
+              <Nav.Link href="#action2" className="me-3 text-center">
+                <HiUsers className="fs-3" />
+                Rete
+              </Nav.Link>
+              <Nav.Link href="#action2" className="me-3 text-center">
+                <BsHandbagFill className="fs-3" />
+                Lavoro
+              </Nav.Link>
+              <Nav.Link href="#action2" className="me-3 text-center">
+                <BiSolidMessageRoundedDots className="fs-3" />
+                Messaggistica
+              </Nav.Link>
+              <Nav.Link href="#action2" className="me-3 text-center">
+                <FaBell className="fs-3" />
+                Notifiche
+              </Nav.Link>
 
-                <NavDropdown title="Tu" id="navbarScrollingDropdown">
-                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-                </NavDropdown>
+              <NavDropdown
+                title={
+                  <span>
+                    <FaUserCircle className="fs-3" /> <br /> Tu
+                  </span>
+                }
+                id="navbarScrollingDropdown"
+              >
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+              </NavDropdown>
 
-                <Container className="border-start ms-3 p-0">
-                  <Button className="bg-transparent text-dark border-0" onClick={handleShow}>
-                    Per le Aziende <FaCaretDown />
-                  </Button>
-                  <Offcanvas show={show} onHide={handleClose} placement="end">
-                    <Offcanvas.Header closeButton>
-                      <Offcanvas.Title>Offcanvas</Offcanvas.Title>
-                    </Offcanvas.Header>
-                    <Offcanvas.Body>
-                      Some text as placeholder. In real life you can have the elements you have chosen. Like, text,
-                      images, lists, etc.
-                    </Offcanvas.Body>
-                  </Offcanvas>
-                </Container>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
+              <Container className="border-start ms-3 p-0">
+                <Button className="bg-transparent text-dark border-0 me-4" onClick={handleShow}>
+                  <BiSolidGrid className="fs-3" />
+                  <br /> Per le Aziende <FaCaretDown />
+                </Button>
+                <Offcanvas show={show} onHide={handleClose} placement="end">
+                  <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                  </Offcanvas.Header>
+                  <Offcanvas.Body>
+                    Some text as placeholder. In real life you can have the elements you have chosen. Like, text,
+                    images, lists, etc.
+                  </Offcanvas.Body>
+                </Offcanvas>
+
+                <Navbar.Text>
+                  <a href="#login">Prova premium per 0 €</a>
+                </Navbar.Text>
+              </Container>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
-      </Navbar>
-    </Container>
+      </Container>
+    </Navbar>
   );
 }
 
