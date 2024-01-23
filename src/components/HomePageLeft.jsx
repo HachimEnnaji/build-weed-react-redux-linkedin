b49b148eb3c86ce76be5940afa41d51ed11308fc
f.guglielmucci <148483575+fg-44@users.noreply.github.com>
2024-01-23 13:03:38 +0100

    update

---
+++ src/components/HomePageLeft.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+/* IMPORTA COMPONENTI */
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+
+import HomePageFooter from "../components/HomePageFooter"
+
+import { BiSolidInfoSquare } from "react-icons/bi";
+import { TbPointFilled } from "react-icons/tb";
+
+function HomePageLeft() {
+  return (
+    <Container>
+      <Col>
+        <Card className="m-0" style={{ width: "18rem" }}>
+          <Card.Header className=" d-flex justify-content-between bg-transparent border-0 ">
+            <Card.Title className="h5 font-weight-bold d-flex align-items-end m-0">
+              Linkedin Notizie
+            </Card.Title>
+            <BiSolidInfoSquare />
+          </Card.Header>
+          <Card.Body>
+            <Container>
+              <Row>
+                <Col>
+                  <Col className="d-flex flex-column " >
+                    
+                      <div className="d-flex">
+                        <TbPointFilled />
+                        <div>
+                        <Card.Title className="h6 m-0">
+                          titolo notizia
+                        </Card.Title>
+                          <Card.Subtitle className="text-muted h6 m-0">
+                            sottotitolo - xxx lettori
+                          </Card.Subtitle>
+                        </div>
+                        </div>
+                  </Col>
+                </Col>
+              </Row>
+            </Container>
+          </Card.Body>
+
+          <Card.Footer className="bg-transparent border-0 m-0">
+            <div className=" d-flex justify-content-center">
+              <Button
+                className=" border-0 bg-transparent text-secondary p-0"
+                size="sm"
+              >
+                Mostra altro
+              </Button>{" "}
+            </div>
+          </Card.Footer>
+        </Card>
+      </Col>
+
+      <Card className="my-2">
+        <Image
+          src="https://placehold.jp/150x150.png"
+          rounded
+          className="border border-warning"
+        />
+      </Card>
+          <Col>
+          <HomePageFooter></HomePageFooter>
+          </Col>
+
+
+
+    </Container>
+  );
+}
+
+export default HomePageLeft;
--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -6,6 +6,7 @@ import Col from "react-bootstrap/Col";
 
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import HomePageLeft from "../components/HomePageLeft";
 
 function HomePages() {
   return (
@@ -33,19 +34,7 @@ function HomePages() {
         </Col>
 
         <Col xs={3}>
-
-        <Card >
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        
+       <HomePageLeft/> 
         </Col>
       </Row>
     </Container>
