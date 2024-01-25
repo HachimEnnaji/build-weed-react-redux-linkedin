4bd0583d263eb4250aa28e1403ac2fcf1aee70dc
DaveGio11 <145689622+DaveGio11@users.noreply.github.com>
2024-01-25 12:11:09 +0100

    12-10-25-CommitDavide

--- src/components/HomePageModalThree.jsx
+++ src/components/HomePageModalThree.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 import { RiArticleLine } from "react-icons/ri";
+import { FaItalic, FaList, FaListOl } from "react-icons/fa";
+import { FaBold, FaLink } from "react-icons/fa6";
+import { ImQuotesRight } from "react-icons/im";
+import { PiBracketsCurlyLight } from "react-icons/pi";
+import { GoDash } from "react-icons/go";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import { Container, Dropdown, DropdownButton } from "react-bootstrap";
 
 function HomePageModalThree() {
   const values = [true];
@@ -23,7 +29,46 @@ function HomePageModalThree() {
       ))}
       <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal</Modal.Title>
+          <Container>
+            <Modal.Title>
+              <div className="d-flex justify-content-between align-items-center">
+                <div className="d-flex align-items-center">
+                  <img
+                    src="https://images.unsplash.com/photo-1607706189992-eae578626c86?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="profile"
+                    width={50}
+                    height={50}
+                    className="rounded-circle object-fit-cover me-2"
+                  />
+                  <div>
+                    <h6 className="mb-1">Nome Cognome</h6>
+                    <p className="fs-7 m-0">Singolo articolo</p>
+                  </div>
+                </div>
+                <div className="bg-light rounded-pill px-3 py-2 d-flex align-items-center">
+                  <Dropdown>
+                    <Dropdown.Toggle variant="light" id="dropdown-basic">
+                      Stile
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                      <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                  <FaBold />
+                  <FaItalic />
+                  <FaList />
+                  <FaListOl />
+                  <ImQuotesRight />
+                  <PiBracketsCurlyLight />
+                  <GoDash />
+                  <FaLink />
+                </div>
+              </div>
+            </Modal.Title>
+          </Container>
         </Modal.Header>
         <Modal.Body>Modal body content</Modal.Body>
       </Modal>
