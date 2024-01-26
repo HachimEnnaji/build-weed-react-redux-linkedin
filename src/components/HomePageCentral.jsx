42d3ed107bc2d61c4a174a85a9a871a8b7c07f73
HachimEnnaji <ashennaji@hotmail.it>
2024-01-26 15:21:02 +0100

    commit 15.21

--- src/components/HomePageCentral.jsx
+++ src/components/HomePageCentral.jsx
@@ -173,15 +173,15 @@ function HomePageCentral() {
             ))}
           </Container>
         ) : (
-          <h3>Nessun post disponibile</h3>
-          // <>
-          //
-          //   <img
-          //     height={600}
-          //     src="https://static.vecteezy.com/system/resources/previews/005/006/031/original/no-result-data-document-or-file-not-found-concept-illustration-flat-design-eps10-modern-graphic-element-for-landing-page-empty-state-ui-infographic-icon-etc-vector.jpg"
-          //     alt="no post found"
-          //   />
-          // </>
+          <>
+            <h3>Nessun post disponibile</h3>
+
+            <img
+              height={600}
+              src="https://static.vecteezy.com/system/resources/previews/005/006/031/original/no-result-data-document-or-file-not-found-concept-illustration-flat-design-eps10-modern-graphic-element-for-landing-page-empty-state-ui-infographic-icon-etc-vector.jpg"
+              alt="no post found"
+            />
+          </>
         )}
       </Row>
     </>
